ba450711c85a0c3e8052c21a686041861ae706d8
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2022-12-23 23:38:23 +0300

    Add project files.

---
+++ QrCodeAPI/Controllers/QrCodeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QrCodeExample;
+using System.Text.Json;
+
+namespace QrCodeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrCodeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GenerateQrCoce([FromQuery] Product product)
+        {
+            string serializedProduct = JsonSerializer.Serialize(product);
+            QrCoder qrCode = new();
+            byte[] generatedQrCode = qrCode.GenerateQrCode(serializedProduct);
+            return File(generatedQrCode, "image/png");
+        }
+    }
+}
---
+++ QrCodeExample/QrCoder.cs
@@ -0,0 +1,16 @@
+﻿using QRCoder;
+
+namespace QrCodeExample
+{
+    public class QrCoder : IQrCoder
+    {
+        public byte[] GenerateQrCode(string value)
+        {
+            QRCodeGenerator qRCodeGenerator = new();
+            QRCodeData data = qRCodeGenerator.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+            PngByteQRCode qrCode = new(data);
+            byte[] byteGraphic = qrCode.GetGraphic(10, new byte[] { 84, 99, 71 }, new byte[] { 240, 240, 240 });
+            return byteGraphic;
+        }
+    }
+}
